36e7237c05110f0042060d71df003a99f6baa14a
Davronbek <davroncoder074@gmail.com>
2024-03-20 13:33:00 +0500

    dfgh

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+let week = prompt("Hafta kunini kiriting!");
+switch (week) {
+  case "dushanba":
+    console.log("monday");
+    break;
+  case "seshanba":
+    console.log("thursday");
+    break
+  case "chorshanba":
+    console.log("wednesday");
+    break
+  case "payshanba":
+    console.log("thorsday");
+    break
+  case "juma":
+    console.log("friday");
+    break
+  case "shanba":
+    console.log("saturday");
+    break
+  case "yakshanba":
+    console.log("sunday");
+    break
+  default:
+    console.log("Bunaqa hafta kuni yuq");
+
+}
